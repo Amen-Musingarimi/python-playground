07cd9118e0eea0974bb257bb52178a875f7e3ca2
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-26 23:55:04 +0200

    Add a for loop

--- numeric.py
+++ numeric.py
@@ -12,3 +12,8 @@ print(a * b)  #36
 print(a / b)  #4.0
 print(a // b) #4 integer division, rounded down towards minus infinity
 print(a % b)  #0 modulo: the remainder after integer division
+
+print()
+
+for i in range(1, 4):
+  print(i)
