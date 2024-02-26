2befdd5b22517ec68f95f2457bc0479e7c5b308c
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-26 23:50:49 +0200

    Add arithmetic operators

--- numeric.py
+++ numeric.py
@@ -2,3 +2,13 @@
 # 1 int - whole numbers with no fractional part
 # 2 float - real numbers with fractional part after the decimal point.
 # 3 complex
+
+a = 12
+b = 3
+
+print(a + b)  #15
+print(a - b)  #9
+print(a * b)  #36
+print(a / b)  #4.0
+print(a // b) #4 integer division, rounded down towards minus infinity
+print(a % b)  #0 modulo: the remainder after integer division
