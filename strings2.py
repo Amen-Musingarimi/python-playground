49b3cacb4d12fcad30b2af3dbc9bbe0d7b455adc
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-27 09:44:34 +0200

    Solve an indexing challenge

---
+++ strings2.py
@@ -0,0 +1,14 @@
+parrot = "Norwegian Blue"
+
+print(parrot)
+
+print(parrot[3])
+
+#Challenge
+print('------Challenge Exercise------')
+print(parrot[3])
+print(parrot[4])
+print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
