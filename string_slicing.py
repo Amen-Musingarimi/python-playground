961e943da854d4ebd5c64cfa14c6f40c82acdd5d
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-28 22:30:59 +0200

    Add string slicing using range

---
+++ string_slicing.py
@@ -0,0 +1,8 @@
+parrot = "Norwegian Blue"
+
+# Slice takes a range up to, but not including the value of a range
+print('-----String Slicing------')
+print(parrot[0:6]) # Norweg
+print(parrot[3: 5])
+print(parrot[0:9]) #Norwegian
+print(parrot[:9]) #Norwegian
