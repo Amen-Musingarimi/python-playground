0e2a809a95f69baa55983f191d3a158b66fa5267
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-28 22:41:58 +0200

    Add slicing without proving start and stop values

--- string_slicing.py
+++ string_slicing.py
@@ -6,3 +6,7 @@ print(parrot[0:6]) # Norweg
 print(parrot[3: 5])
 print(parrot[0:9]) #Norwegian
 print(parrot[:9]) #Norwegian
+print(parrot[10:]) #Blue
+
+print(parrot[:6] + parrot[6:])  
+print(parrot[:])
