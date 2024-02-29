a6996f326f07496a28a6d96b54520c5e96301ce2
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-29 22:29:01 +0200

    Reorder code

--- sliceback.py
+++ sliceback.py
@@ -5,9 +5,10 @@ print(backwards)
 
 print('------Challenge Exercise------')
 # Create a slice that produces the characters qpo
-# Slice the string to produce edcba
-# Slice the string to produce the last 8 characters, in reverse order
-
 print(letters[16:13:-1]) #qpo
+
+# Slice the string to produce edcba
 print(letters[4::-1]) #edcba
-print(letters[25:17:-1])
+
+# Slice the string to produce the last 8 characters, in reverse order
+print(letters[25:17:-1]) #last eight letters in reverse order
