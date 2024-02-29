6d48f3d9749a67a5af1ed3de82f227d36d478dd8
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-29 22:18:10 +0200

    Solved the slicing challenge

---
+++ sliceback.py
@@ -0,0 +1,13 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[25::-1]
+print(backwards)
+
+print('------Challenge Exercise------')
+# Create a slice that produces the characters qpo
+# Slice the string to produce edcba
+# Slice the string to produce the last 8 characters, in reverse order
+
+print(letters[16:13:-1]) #qpo
+print(letters[4::-1]) #edcba
+print(letters[25:17:-1])
