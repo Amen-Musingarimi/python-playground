c052ae45d4e63c546be8d63f2b6318d88cb53561
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-02-29 23:03:14 +0200

    Add string concatnation and multiplication

---
+++ string_operators.py
@@ -0,0 +1,18 @@
+#Python 3 has 5 sequence types built in:
+  #The string type
+  #list
+  #tuple
+  #range
+  #bytes and bytearray
+
+#A sequence is defined as an ordered set of items
+string1 = "He's "
+string2 = "probably "
+string3 = "pinning "
+string4 = "for the "
+string5 = "fjords."
+
+print(string1 + string2 + string3 + string4 + string5)
+
+#Strings can also be multiplied in python
+print("Hello " * 5)
