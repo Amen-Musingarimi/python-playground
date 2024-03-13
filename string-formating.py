afdf18e8ca5a3969e887d9d266cadc57e55f641e
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-13 16:43:14 +0200

    Add formating decimal numbers

--- string-formating.py
+++ string-formating.py
@@ -11,3 +11,13 @@ print('---------------------------')
 # Format Center Aligned
 for i in range(1, 13):
   print("No. {0:2} squared is {1:<3} and cubed is {2:^4}".format(i, i ** 2, i ** 3))
+
+print('---------------------------')
+# Format Floating numbers. We use f to specify the number of decimals needed.
+print("Pi is approximately {0:12}".format(22 / 7))
+print("Pi is approximately {0:12f}".format(22 / 7))
+print("Pi is approximately {0:12.50f}".format(22 / 7))
+print("Pi is approximately {0:52.50f}".format(22 / 7))
+print("Pi is approximately {0:62.50f}".format(22 / 7))
+print("Pi is approximately {0:72.50f}".format(22 / 7))
+print("Pi is approximately {0:12.2f}".format(22 / 7))
