18ca36a0814379d5ff9d1a9beb34c0b7d2bef84d
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-15 14:59:21 +0200

    Show effects of block code using a loop

--- blocks-and-statements/blocks.py
+++ blocks-and-statements/blocks.py
@@ -1,3 +1,14 @@
 for i in range(1, 13):
   if(i % 2 == 0):
     print(i)
+# Show effects of indentation
+    
+# Print the * after printing the numbers in a range
+for i in range(1, 13):
+  print(i)
+print('~' * 60)
+
+# Print the * after each number in a range
+for i in range(1, 13):
+  print(i)
+  print('~' * 60)
