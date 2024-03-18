e44ffc4289ec656030771414ac93e0936caa725f
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-18 23:46:41 +0200

    Change the if statement structure but producing the same results

--- blocks-and-statements/ifStatements.py
+++ blocks-and-statements/ifStatements.py
@@ -8,3 +8,10 @@ if age >= 18:
   print("Please put an X in the box")
 else:
   print("Please come back in {0} years".format(18 - age))
+
+
+if age < 18:
+  print("Please come back in {0} years".format(18 - age))
+else:
+  print("You are old enough to vote")
+  print("Please put an X in the box")
