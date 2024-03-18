a0b0cfe19190b88f069590bac9313f24fb59f6a2
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-18 23:57:40 +0200

    Addd a guessing game

---
+++ blocks-and-statements/guessingGame.py
@@ -0,0 +1,9 @@
+answer = 5
+
+print("Please guess a number between 1 and 10: ")
+guess = int(input())
+
+if guess < answer:
+  print("Your guess is too low. Please guess higher.")
+elif guess > answer:
+  print("Your guess is too high. Please guess lower.")
