d4aafb59b345defb89015c0d332419e835a518d2
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-19 00:05:13 +0200

    Add nested if statements

--- blocks-and-statements/guessingGame.py
+++ blocks-and-statements/guessingGame.py
@@ -5,7 +5,13 @@ guess = int(input())
 
 if guess < answer:
   print("Your guess is too low. Please guess higher.")
+  guess = int(input())
+  if guess == answer:
+    print("Well done, you guessed it right.")
 elif guess > answer:
   print("Your guess is too high. Please guess lower.")
+  guess = int(input())
+  if guess == answer:
+    print("Well done, you guessed it right.")
 else:
   print("You got it first time.")
