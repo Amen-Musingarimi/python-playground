a963e0c8997cdb2b33c80edf8150093c93bfe0a3
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-19 23:09:49 +0200

    Add conditions for wrong guess

--- conditionalOperators.py
+++ conditionalOperators.py
@@ -3,19 +3,26 @@ answer = 5
 print("Please guess a number between 1 and 10: ")
 guess = int(input())
 
-if guess < answer:
-  print("Your guess is too low. Please guess higher.")
-  guess = int(input())
-  if guess == answer:
-    print("Well done, you guessed it right.")
+if guess != answer:
+  if guess < answer:
+    print("Please guess higher.")
   else:
-    print("Sorry, you have not guessed correctly.")
-elif guess > answer:
-  print("Your guess is too high. Please guess lower.")
-  guess = int(input())
-  if guess == answer:
-    print("Well done, you guessed it right.")
-  else:
-    print("Sorry, you have not guessed correctly.")
-else:
-  print("You got it first time.")
+    print("Please guess lower.")
+
+
+# if guess < answer:
+#   print("Your guess is too low. Please guess higher.")
+#   guess = int(input())
+#   if guess == answer:
+#     print("Well done, you guessed it right.")
+#   else:
+#     print("Sorry, you have not guessed correctly.")
+# elif guess > answer:
+#   print("Your guess is too high. Please guess lower.")
+#   guess = int(input())
+#   if guess == answer:
+#     print("Well done, you guessed it right.")
+#   else:
+#     print("Sorry, you have not guessed correctly.")
+# else:
+#   print("You got it first time.")
