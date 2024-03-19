09a6e03ce91331411dba2ed4261bc0ae664a7724
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-19 23:06:40 +0200

    Setup code for conditional operators

---
+++ conditionalOperators.py
@@ -0,0 +1,21 @@
+answer = 5
+
+print("Please guess a number between 1 and 10: ")
+guess = int(input())
+
+if guess < answer:
+  print("Your guess is too low. Please guess higher.")
+  guess = int(input())
+  if guess == answer:
+    print("Well done, you guessed it right.")
+  else:
+    print("Sorry, you have not guessed correctly.")
+elif guess > answer:
+  print("Your guess is too high. Please guess lower.")
+  guess = int(input())
+  if guess == answer:
+    print("Well done, you guessed it right.")
+  else:
+    print("Sorry, you have not guessed correctly.")
+else:
+  print("You got it first time.")
