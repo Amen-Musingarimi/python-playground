a473c87e166ac9c4903a25fa626c0225b6822b2c
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-20 10:43:35 +0200

    Use or in a conditional operator

--- blocks-and-statements/orAnd.py
+++ blocks-and-statements/orAnd.py
@@ -5,3 +5,11 @@ if 16 <= age <= 65:
   print("Have a good day at work.")
 else: 
   print("Enjoy your free time") 
+
+
+print("-" * 70)
+
+if age < 16 or age > 65:
+  print("Enjoy your free time.")
+else:
+  print("Have a good day at work")
