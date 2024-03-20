c6df645133a9446fea3736fc74b031db5188f573
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-20 13:03:15 +0200

    Add picture of if challenge

--- blocks-and-statements/ifChallenge.py
+++ blocks-and-statements/ifChallenge.py
@@ -3,6 +3,6 @@ name = input("What is your name?: ")
 age = int(input("How old are you?: "))
 
 if age >= 18 and age <= 30:
-  print("You can go for the 18-30 holiday.")
+  print("{}, welcome to the 18-30 holiday.".format(name))
 else:
   print("You are not authorized to go for the 18-30 holiday.")
