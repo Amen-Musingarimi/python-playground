087636bbe452ec5ea79801dede3fba4bd420106b
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-20 12:46:40 +0200

    Use not in function

---
+++ blocks-and-statements/notIn.py
@@ -0,0 +1,8 @@
+activity = input("What would you like to do today? ")
+
+if "cinema" not in activity.casefold():
+  print("But I want to go to the cinema.")
+
+
+# casefold() converts the first letter of a string to an Uppercase and the rest to lowercase
+  
