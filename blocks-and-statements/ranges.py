6fc2a83c6a7d9df2654f752226c963703a159624
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-20 17:46:59 +0200

    Add for loop in a range without specifying the start value

--- blocks-and-statements/ranges.py
+++ blocks-and-statements/ranges.py
@@ -1,2 +1,9 @@
-for i in range(1, 20):
-  print("i is now {}".format(i))
+# providing a start and end value
+for i in range(1, 6):
+  print("i is now {}".format(i)) 
+
+print("*" * 60)
+
+# providing an end value. The start value defaults to zero
+for i in range(5):
+  print("i is now {}".format(i)) 
