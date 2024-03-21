35e368b569c5ead31bd10d28b60dea095a3f0dd1
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-21 11:40:18 +0200

    Add a while loop

---
+++ blocks-and-statements/whileLoop.py
@@ -0,0 +1,6 @@
+# The while loop continues to loop as long as the condition is true and stops when the condition becomes false it stops looping.
+i = 0
+
+while i < 10:
+  print("i is now {}.".format(i))
+  i += 1 
