6db6d2c5cb44a45b8f755e2075293182449ec230
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-21 13:11:31 +0200

    Use a while loop to improve the guessing game

--- blocks-and-statements/guessing-game-improved.py
+++ blocks-and-statements/guessing-game-improved.py
@@ -3,23 +3,17 @@ import random
 highest = 10
 answer = random.randint(1, highest)
 print(answer)
-
 print("Please guess a number between 1 and {}: ".format(highest))
-guess = int(input())
+guess = 0 # Initialize to any number that doesn't equal the answer.
 
-if guess < answer:
-  print("Your guess is too low. Please guess higher.")
+while guess != answer:
   guess = int(input())
+
   if guess == answer:
-    print("Well done, you guessed it right.")
+      print("Well done, you guessed it right.")
   else:
-    print("Sorry, you have not guessed correctly.")
-elif guess > answer:
-  print("Your guess is too high. Please guess lower.")
-  guess = int(input())
-  if guess == answer:
-    print("Well done, you guessed it right.")
-  else:
-    print("Sorry, you have not guessed correctly.")
-else:
-  print("You got it first time.")
+      if guess < answer:
+        print("Your guess is too low. Please guess higher.")
+      else:
+        print("Your guess is too high. Please guess lower.")
+   
