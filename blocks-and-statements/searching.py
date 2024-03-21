b323ac753a728326ac83fbce3f9fdb4d32f4485c
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-21 11:17:16 +0200

    Add a break function in the loop

--- blocks-and-statements/searching.py
+++ blocks-and-statements/searching.py
@@ -3,9 +3,11 @@ shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 item_to_find = "spam"
 found_at = None # Note that None represents a variable without a value.It is equvalent to Null in Java, C and JavaScript
 
-for index in range(len(shopping_list)):
+for index in range(len(shopping_list)): #Note that len() is a python inbuilt fuction that returns length of the varialbe
   if shopping_list[index] == item_to_find:
     found_at = index
+    break #Break is important to break out of the loop when the condition we have set has been met. It prevents the iteration to continue.
 
 print(found_at)
 print(shopping_list[found_at])
+print(len("Takudzwa"))
