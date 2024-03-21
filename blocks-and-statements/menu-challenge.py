c1188830176106003f2e2c6d4cf3d1742c7b99ae
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-22 01:11:39 +0200

    Add a menu challenge

---
+++ blocks-and-statements/menu-challenge.py
@@ -0,0 +1,18 @@
+menu_list = ["Learn Python", "Learn Java", "Go Swimming", "Have Dinner", "Go To Bed"]
+
+print("Please choose your option from the list below:")
+for index, option in enumerate(menu_list, start=1):
+    print(f"{index}. {option}")
+
+choice = None
+
+while True:
+    choice = int(input("Enter the number that correspond to your choice: "))
+    if choice == 0:
+        print("Exiting the program...")
+        break
+        
+    else:
+        print("Good luck, you can go {}".format(menu_list[choice - 1]))
+
+    
