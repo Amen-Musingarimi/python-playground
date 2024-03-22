0ec0400ac8f0e7c1d0b6a4a120b228986075bbdd
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-22 17:05:17 +0200

    Add sequence types notes

---
+++ sequence-types/sequence.py
@@ -0,0 +1,7 @@
+# A sequence is an ordered collection of items.
+
+# Note the word ordered is important
+
+# When you iterate over a sequence - using a for loop, for example - you will always get the items in the same order.
+
+# A sequence types can be iterated over, e.g strings, lists, etc.
