6094678e4759283dfe5b953a3505c3c780cd0082
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-25 16:07:29 +0200

    Add immutable notes

--- sequence-types/sequence-types-notes.py
+++ sequence-types/sequence-types-notes.py
@@ -7,3 +7,12 @@
 # A sequence types can be iterated over, e.g strings, lists, etc.
 
 # Indexing and Slicing works the same on both Strings and Lists because they are both sequence types. But the main difference between the 2 is strings are immutable(they cannot be changed) while lists are mutable(can be changed)
+
+# List of immutable types in Python:
+  #1 Int 
+  #2 Float
+  #3 bool (true/false): a subclass of int
+  #4 str (string)
+  #5 tuple
+  #6 frozenset
+  #7 bytes
