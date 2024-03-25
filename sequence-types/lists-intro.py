1a7226727df63ca023feb673418d9e168a175784
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-25 15:52:37 +0200

    List indexing

--- sequence-types/lists-intro.py
+++ sequence-types/lists-intro.py
@@ -1,4 +1,9 @@
 computer_parts = ["computer", "monitor", "keyboard", "mouse", "mouse mat"]
 
+# Looping through the list
 for part in computer_parts:
   print(part)
+
+# Indexing the list
+print("-" * 60)
+print(computer_parts[2])
