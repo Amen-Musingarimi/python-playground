ebfcfa68a532c8239400f4f5a54de96ff8841688
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-25 16:01:21 +0200

    Add notes

--- sequence-types/sequence.py
+++ sequence-types/sequence.py
@@ -5,3 +5,5 @@
 # When you iterate over a sequence - using a for loop, for example - you will always get the items in the same order.
 
 # A sequence types can be iterated over, e.g strings, lists, etc.
+
+# Indexing and Slicing works the same on both Strings and Lists because they are both sequence types. But the main difference between the 2 is strings are immutable(they cannot be changed) while lists are mutable(can be changed)
