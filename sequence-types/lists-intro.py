6272bce7ad0a35e7283c02d6f89d828cb5e07229
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-25 15:56:07 +0200

    Add list slicing

--- sequence-types/lists-intro.py
+++ sequence-types/lists-intro.py
@@ -7,3 +7,8 @@ for part in computer_parts:
 # Indexing the list
 print("-" * 60)
 print(computer_parts[2])
+
+# Slicing the list. It produces another list in return
+print("-" * 60)
+print(computer_parts[0:3]) #Returns first 3 items in a list
+print(computer_parts[-1]) #Returns the last item in a list
