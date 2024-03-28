fa15257fa16ef2cff8a6e5d1f6af25e1cb01be1b
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-03-28 22:16:07 +0200

    Replace list item using slicing

--- sequence-types/list_operations.py
+++ sequence-types/list_operations.py
@@ -2,6 +2,11 @@ computer_parts = ["computer", "monitor", "keyboard", "mouse", "mouse mat", "hdmi
 
 print(computer_parts)
 
+print("---------------------REPLACING LIST ITEMS USING INDEXING-----------------------")
 computer_parts[3] = "trackball"
+print(computer_parts)
 
+print("---------------------REPLACING LIST ITEMS USING SLICING-----------------------")
+print(computer_parts[3:]) # Prints the list item from index position 3 to the end
+computer_parts[3:] = ["mouse"] # Replaces all items in the list from the index 3 up to the end with one list item "mouse"
 print(computer_parts)
