6a844da14678abeddff9b3213c958aad037a6edb
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-01 10:51:06 +0200

    Modify loop to delete list items out of the defined range

--- sequence-types/delete-list-item-backwards.py
+++ sequence-types/delete-list-item-backwards.py
@@ -4,4 +4,8 @@ min_valid = 100
 max_valid = 200
 
 for index in range(len(data) - 1, -1, -1):
-  print(index)
+  if data[index] < min_valid or data[index] > max_valid:
+    print(index, data)
+    del data[index]
+
+print(data)
