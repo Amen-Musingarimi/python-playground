c4503f1354b302b912b571c2f4e35c7ecc9e7b46
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-01 21:32:45 +0200

    Demostrate how the function split() works

---
+++ sequence-types/splitting_things.py
@@ -0,0 +1,6 @@
+panagram = "The quick brown fox jumps over the lazy dog"
+
+# Split() returns a list
+words = panagram.split() # We did not specify any arguments to the split function so it defaults to splitting on white spaces.
+
+print(words)
