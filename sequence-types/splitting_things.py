3fdbf6e0f198105c823d8eb853306a46e3520153
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-01 21:35:02 +0200

    Split numbers when we specify where to split

--- sequence-types/splitting_things.py
+++ sequence-types/splitting_things.py
@@ -4,3 +4,6 @@ panagram = "The quick brown fox jumps over the lazy dog"
 words = panagram.split() # We did not specify any arguments to the split function so it defaults to splitting on white spaces.
 
 print(words)
+
+numbers = "9,223,372,036,854,775,807"
+print(numbers.split(","))
