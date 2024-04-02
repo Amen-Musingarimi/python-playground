b979f0fc965adda02095b62764a436fe570acabf
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-02 23:59:45 +0200

    Add more nested tuples

--- sequence-types/tuples_nested_further.py
+++ sequence-types/tuples_nested_further.py
@@ -1,6 +1,16 @@
 albums = [
-          ("Welcome to my Nightmare", "Alice Cooper", 1975),
-          ("Bad Company", "Bad Company", 1974),
+          ("Welcome to my Nightmare", "Alice Cooper", 1975,
+           [
+             (1, "Welcome to my Nightmare")
+             (2, "Devel's Food")
+             (3, "The Black Widow")
+             (4, "Some Folks")
+             (5, "Only Women Bleed")
+           ]),
+          ("Bad Company", "Bad Company", 1974,
+           [
+             
+           ]),
           ("Nightflight", "Budgie", 1981),
           ("More Mayhem", "Emilda May", 2011),
           ("Ride The Lightning", "Metallica", 1984)
