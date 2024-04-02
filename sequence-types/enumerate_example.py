0653db55d2f43abaaa7756bac10fed2181084958
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-02 20:55:29 +0200

    Demostrate how enumerate work with tuples

--- sequence-types/enumerate_example.py
+++ sequence-types/enumerate_example.py
@@ -1,2 +1,9 @@
+# Demostrate that enumerate() returns tuples of the character and its index value.
+print("------------------------Return tuples-------------------------")
+for char in enumerate("abcdefgh"):
+  print(char)
+
+print("------------------------Return unpacked tuples-------------------------")
+# Demostrate unpacking a tuple for the index and tuple item to be returned seperately
 for index, character in enumerate("abcdefgh"):
   print(index, character)
