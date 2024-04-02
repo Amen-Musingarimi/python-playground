f4daebc7d8c9a8b4676093d157126601f07d4d6d
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-02 23:53:02 +0200

    Create a new file handling further tuple nesting

---
+++ sequence-types/tuples_nested_further.py
@@ -0,0 +1,7 @@
+albums = [
+          ("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride The Lightning", "Metallica", 1984)
+        ]
