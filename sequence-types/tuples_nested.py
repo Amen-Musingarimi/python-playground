e2eb2cd30b1cc56570bd6ef082b54403e0668290
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-02 23:41:24 +0200

    Show how netsed tuples work

---
+++ sequence-types/tuples_nested.py
@@ -0,0 +1,13 @@
+albums = [
+          ("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride The Lightning", "Metallica", 1984)
+        ]
+
+
+print(len(albums))
+
+for album in albums:
+  print("Album: {}, Artist: {}, Year: {}".format(album[0], album[1], album[2]))
