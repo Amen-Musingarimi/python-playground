5d917fb9adaeaf0f93a76e90df2fa0d228271377
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-02 20:43:53 +0200

    Demostrate unpacking a list

--- sequence-types/tuples_unpacking.py
+++ sequence-types/tuples_unpacking.py
@@ -5,3 +5,19 @@ x, y, z = 1, 2, 76
 print(x)
 print(y)
 print(z)
+
+print("----------------Unpacking a Tuple-----------------")
+data_tuple = (3, 11, 25)
+t, a, f = data_tuple
+
+print(t)
+print(a)
+print(f)
+
+print("----------------Unpacking a List-----------------")
+data_list = (3, 95, 91)
+e, t, s = data_list
+
+print(e)
+print(t)
+print(s)
