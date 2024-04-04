6b3007aff51a01399108c96c5f1d1cc50751d0b9
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-04 23:52:02 +0200

    Add a guessing game with old ordinary code

---
+++ functions/guessing_game.py
@@ -0,0 +1,35 @@
+import random
+
+highest = 10
+chances = 5
+answer = random.randint(1, highest)
+print(answer)
+print("Please guess a number between 1 and {}: ".format(highest))
+guess = 0  # Initialize to any number that doesn't equal the answer.
+
+while guess != answer:
+    guess = int(input())
+
+    if guess == 0:
+        break
+
+    if guess == answer:
+        print("Well done, you guessed it right.")
+    else:
+        if guess < answer:
+            print("Your guess is too low. Please guess higher.")
+            # chances = chances - 1
+            chances -= 1
+            print("{} remaining chances.".format(chances))
+            if chances < 1:
+                print("Game Over! You lost the game.")
+                break
+        else:
+            print("Your guess is too high. Please guess lower.")
+            # chances = chances - 1
+            chances -= 1
+            print("{} remaining chances.".format(chances))
+            if chances < 1:
+                print("Game Over! You lost the game.")
+                break
+
