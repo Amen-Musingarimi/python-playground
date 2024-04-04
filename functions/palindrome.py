a9940f24d306e126c8bd488688bcd1955b8f58f2
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-04 22:20:39 +0200

    Clean up code

--- functions/palindrome.py
+++ functions/palindrome.py
@@ -1,14 +1,14 @@
 # A palindrome is a word that reads the same backwards as forwards.
 
 def is_palindrome(string):
-    backwards = string[::-1].lower()
+    backwards = string[::-1]
     print(backwards)
-    return backwards == string
+    return backwards.lower() == string.lower()
 
 
 word = input("Please enter a word to check: ")
 
-if is_palindrome(word.lower()):
-    print("'{}' is a palindrome.".format(word.casefold()))
+if is_palindrome(word):
+    print("'{}' is a palindrome.".format(word))
 else:
-    print("'{}' is not a palindrome.".format(word.casefold()))
+    print("'{}' is not a palindrome.".format(word))
