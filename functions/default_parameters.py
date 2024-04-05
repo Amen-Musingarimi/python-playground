4550262ae5a513ee57a70af4a3adf35991033ab6
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-05 10:57:32 +0200

    Reformat file

--- functions/default_parameters.py
+++ functions/default_parameters.py
@@ -4,4 +4,4 @@ def calc_rectangle_area(length=15, width=10):
 
 
 print(calc_rectangle_area())  # We call the function without parameters. It will be executed with default values.
-print(calc_rectangle_area(20, 10)) # We specify the parameter values, which overwrites the default values.
+print(calc_rectangle_area(20, 10))  # We specify the parameter values, which overwrites the default values.
