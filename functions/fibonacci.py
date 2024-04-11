0f82869dc3ef1808f881e12c70188062db9329a7
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-11 11:42:08 +0200

    Add a fibonacci function

--- functions/fibonacci.py
+++ functions/fibonacci.py
@@ -4,3 +4,13 @@ def fibonacci(n):
         return n
 
     n_minus1, n_minus2 = 1, 0
+
+    result = None
+    for f in range(n - 1):
+        result = n_minus2 + n_minus1
+        n_minus2 = n_minus1
+        n_minus1 = result
+
+    return result
+
+
