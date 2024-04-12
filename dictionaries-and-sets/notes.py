2f14726cd808b306699ab8731080059cb65692f0
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-12 12:31:50 +0200

    Add an example dictionary

--- dictionaries-and-sets/notes.py
+++ dictionaries-and-sets/notes.py
@@ -2,4 +2,5 @@
 # Dictionaries and sets are not sequences therefore we cannot access the items using index positions.
 # When dealing with items in a dictionary, we use a key to get individual values from the dictionary.
 # Dictionaries store key/value pairs.
+# A dictionary is a collection of values, that are stored using a key.
 # A set is an unordered collection of things. There is no way to retrieve a specific item from a set.
