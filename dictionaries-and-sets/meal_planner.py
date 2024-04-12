d3358d92249a06b7a2f1b6bb5fcaa13f25280f5a
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-12 21:14:27 +0200

    Implement display menu and store selected item in a variable

--- dictionaries-and-sets/meal_planner.py
+++ dictionaries-and-sets/meal_planner.py
@@ -2,5 +2,21 @@ from content import pantry, recipes
 
 display_dict = {}
 for index, key in enumerate(recipes):
-    print(index, key)
+    # print(index, key)
+    display_dict[str(index + 1)] = key
+# print(display_dict)
 
+while True:
+    # Display a menu of the recipes  we know how to cook
+    print("Please choose your recipe")
+    print("-------------------------")
+    for key, value in display_dict.items():
+        print(f"{key} - {value}")
+
+    choice = input(": ")
+
+    if choice == "0":
+        break
+    elif choice in display_dict:
+        selected_item = display_dict[choice]
+        print(selected_item)
