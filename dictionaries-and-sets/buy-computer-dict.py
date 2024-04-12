f5b3dcf26e15b6ca89ab97219ec6676421f6e67d
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-12 18:04:10 +0200

    Implement adding and removing items to computer parts dictionary

--- dictionaries-and-sets/buy-computer-dict.py
+++ dictionaries-and-sets/buy-computer-dict.py
@@ -13,8 +13,14 @@ while current_choice != "0":
     if current_choice in available_parts:
         # Note that `in` checks for the keys in the dictionary not the values.
         chosen_part = available_parts[current_choice]
-        print(f"Adding {chosen_part}")
-
+        if current_choice in computer_parts:
+            # It's already in, so remove it
+            print(f"Removing {chosen_part}")
+            computer_parts.pop(current_choice)
+        else:
+            print(f"Adding {chosen_part}")
+            computer_parts[current_choice] = chosen_part
+        print(f"Your dictionary now contains: {computer_parts}")
     else:
         print("Please add options from the list below:")
         for key, value in available_parts.items():
