73ef34fd0000a590a13c1aa954289979aa7e8d8a
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-16 14:31:32 +0200

    Explore how dictionary defaults work

---
+++ dictionaries-and-sets/dict_defaults.py
@@ -0,0 +1,7 @@
+from content import  pantry
+
+chicken_quantity = pantry.setdefault("chicken", 0)
+print(f"chicken: {chicken_quantity}")
+
+beans_quantity = pantry.setdefault("beans", 0)
+print(f"beans: {beans_quantity}")
