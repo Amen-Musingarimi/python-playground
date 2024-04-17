3b10d74bca422c9eb2848aa106cd10ef1d776a96
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-17 18:50:19 +0200

    Update a dict with values from a list

--- dictionaries-and-sets/dict-methods/update.py
+++ dictionaries-and-sets/dict-methods/update.py
@@ -1,4 +1,4 @@
-from data import d
+from data import d, pantry_items
 
 # The update() method -----------------------------------
 # The update() is used to update the contents of a dictionary with elements from another dictionary or from an
@@ -18,5 +18,11 @@ d.update(d2)
 print(d)
 
 print("-----------Iterate over the newly updated 'd' dict--------------")
+for key, value in d.items():
+    print(f"{key}: {value}")
+
+print("-----------Update the original `d` with a different data type, a list--------------")
+print(pantry_items)
+d.update(enumerate(pantry_items))
 for key, value in d.items():
     print(f"{key}: {value}")
