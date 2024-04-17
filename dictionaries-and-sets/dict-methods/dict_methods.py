3b01fc15e4e44f44dda041b8c8f5bf005874d7d5
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-17 17:51:08 +0200

    Demostrate specifying the default value and not specifying

--- dictionaries-and-sets/dict-methods/dict_methods.py
+++ dictionaries-and-sets/dict-methods/dict_methods.py
@@ -1,21 +1,10 @@
-d = {
-    0: "zero",
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-}
-
-pantry_items = ['chicken', 'spam', 'egg', 'bread', 'lemon']
+from data import pantry_items
 
 # THE fromkeys() METHOD: ----------------------------------- fromkeys() is method that creates a new dictionary with
 # keys from a provided iterable and assigns each key a default value. is useful when you want to initialize a
 # dictionary with a predefined set of keys, all having the same initial value. It expects two arguments,
 # the iterable and the default value. If the default value is not specified `None` will be assigned as a default value
-new_dict = dict.fromkeys(pantry_items)
+new_dict = dict.fromkeys(pantry_items)  # With no specified default value.
+second_dict = dict.fromkeys(pantry_items, 0)  # 0 will be the default value for every key
 print(new_dict)
+print(second_dict)
