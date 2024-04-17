dd8ed87ae61995ce13c12f90ba62f5487edbc3f8
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-04-17 18:24:54 +0200

    Add notes for the keys() method

--- dictionaries-and-sets/dict-methods/keys.py
+++ dictionaries-and-sets/dict-methods/keys.py
@@ -1,4 +1,10 @@
 from data import d
 
+# The keys() method --------------------------
+# The keys() method returns a view object that displays a list of all the keys in the dictionary
+# This method doesn't take any parameters. It simply returns a view object that provides a
+# dynamic view of the dictionary's keys. A view object behaves like a set, meaning that you can iterate over it and
+# perform set operations such as membership tests
+
 keys = d.keys()
 print(keys)
