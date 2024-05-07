64a07ec842e463a6eefc8d0b4915dd8e27743c1a
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-05-07 16:16:29 +0200

    Add more notes on indexing

--- dictionaries-and-sets/sets/sets_notes.py
+++ dictionaries-and-sets/sets/sets_notes.py
@@ -1,3 +1,4 @@
 # The keys of a dictionary are very similar to a set. The main difference is that dictionary keys are ordered.
 # Sets have no ordering.
 # Items only appear once in a union set. The elements are unique.
+# Sets are unordered. This means we cannot index a set. Items in a set don't have a fixed position.
