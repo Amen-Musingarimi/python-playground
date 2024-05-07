abbda786c9f1a92ebe8b35579e9a3528c8efdd1c
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-05-07 20:44:17 +0200

    Check if sets are equal

--- dictionaries-and-sets/sets/sets_intro.py
+++ dictionaries-and-sets/sets/sets_intro.py
@@ -3,3 +3,10 @@ print(farm_animals)
 
 for animal in farm_animals:
     print(animal)
+
+more_animals = {'sheep', 'goat', 'horse', 'cow', 'hen'}
+
+if farm_animals == more_animals:
+    print('The sets are equal')
+else:
+    print('The sets are different')
