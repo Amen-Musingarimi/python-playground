3a193599f828b93e4ba3908538760999e5743059
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-05-07 15:56:26 +0200

    Add more notes

--- dictionaries-and-sets/sets/sets_notes.py
+++ dictionaries-and-sets/sets/sets_notes.py
@@ -1,2 +1,3 @@
 # The keys of a dictionary are very similar to a set. The main difference is that dictionary keys are ordered.
 # Sets have no ordering.
+# Items only appear once in a union set. The elements are unique.
