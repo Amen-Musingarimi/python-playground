d99f1db7bd9da23989a7c818b67bb6af69fea934
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-05-07 20:46:01 +0200

    Add more notes

--- dictionaries-and-sets/sets/sets_notes.py
+++ dictionaries-and-sets/sets/sets_notes.py
@@ -2,3 +2,4 @@
 # Sets have no ordering.
 # Items only appear once in a union set. The elements are unique.
 # Sets are unordered. This means we cannot index a set. Items in a set don't have a fixed position.
+# Python considers the sets to be equal if they contain the same items in them, the order isn't important.
