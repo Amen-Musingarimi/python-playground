abebb13a0e8b744ae6679ef75c13d6183c650625
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-05-08 10:05:11 +0200

    Demostrate the remove method

--- dictionaries-and-sets/sets/sets_notes.py
+++ dictionaries-and-sets/sets/sets_notes.py
@@ -5,3 +5,5 @@
 # Python considers the sets to be equal if they contain the same items in them, the order isn't important.
 # ############# Set Methods ######################
 # clear() - clears every item in a set to leave an empty set.
+# discard() - removes a specified item from the set. Nothing changes if the specified item is not in the set.
+# remove() - removes a specified item from the set. If nothing is specified it returns an error, the programme crashes.
