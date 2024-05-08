8be39442a905466cb8b2d64e03e39863608d0d0f
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-05-08 09:46:54 +0200

    Demostrate the discard() method

--- dictionaries-and-sets/sets/remove_item_set.py
+++ dictionaries-and-sets/sets/remove_item_set.py
@@ -1,5 +1,10 @@
 small_ints = set(range(21))
 print(small_ints)
 
-small_ints.clear()
+# Removes all items in a set.
+# small_ints.clear()
+# print(small_ints)
+
+# Removes the specified item from the set.
+small_ints.discard(10)
 print(small_ints)
